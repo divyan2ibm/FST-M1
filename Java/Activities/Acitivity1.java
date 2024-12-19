3e7e806a7a1f422eb4c952fa6c06aa2b013954c9
divyan2ibm <divya.n2@IBM.com>
2024-12-19 10:31:26 +0530

    Update Acitivity1.java

--- Java/Activities/Acitivity1.java
+++ Java/Activities/Acitivity1.java
@@ -1,18 +1,18 @@
 public class Acitivity1 {
     public static void main(String[] args) {
-        // Create a Car object
+
         Car myCar = new Car("Red", "Automatic", 2020);
 
-        // Display the characteristics of the car
+        
         myCar.displayCharacteristics();
 
-        // Call accelerate and brake methods
+        
         myCar.accelerate();
         myCar.brake();
     }
 
 
-		// TODO Auto-generated method stub
+		
 
 	}
 
