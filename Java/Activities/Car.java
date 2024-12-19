45d2986005fbe45e8fcd1aff46f6768a5317580d
divyan2ibm <divya.n2@IBM.com>
2024-12-19 10:32:53 +0530

    Update Car.java

--- Java/Activities/Car.java
+++ Java/Activities/Car.java
@@ -1,21 +1,21 @@
 public class Car {
-    // Instance variables
+    
     private String color;
     private String transmission;
     private int make;
     private int tyres;
     private int doors;
 
-    // Constructor to initialize tyres and doors
+    
     public Car(String color, String transmission, int make) {
         this.color = color;
         this.transmission = transmission;
         this.make = make;
-        this.tyres = 4;  // Default value
-        this.doors = 4;  // Default value
+        this.tyres = 4;  
+        this.doors = 4;  
     }
 
-    // Method to display car characteristics
+    
     public void displayCharacteristics() {
         System.out.println("Car Characteristics:");
         System.out.println("Color: " + color);
@@ -25,12 +25,12 @@ public class Car {
         System.out.println("Doors: " + doors);
     }
 
-    // Method to simulate accelerating the car
+    
     public void accelerate() {
         System.out.println("Car is moving forward.");
     }
 
-    // Method to simulate braking the car
+    
     public void brake() {
         System.out.println("Car has stopped.");
     }
